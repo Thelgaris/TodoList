91956e100431634c47ef9c28f6fb3673b3f835e7
Thelgaris <alex.gabarron84@gmail.com>
2022-04-05 19:31:55 +0000

    secondcommit

---
+++ src/js/component/Todo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Todo = () => {
+	return (
+		<div className="mb-3 container">
+			<input
+				type="email"
+				class="form-control"
+				id="exampleFormControlInput1"
+				placeholder="Write your shit to do buddy"
+			/>
+		</div>
+	);
+};
+
+export default Todo;
---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TodoList = () => {
+	return (
+		<div>
+			<div>tell</div>
+			<div>tell</div>
+			<div>tell</div>
+			<div>tell</div>
+			<div>tell</div>
+			<div>tell</div>
+			<div>tell</div>
+			<div>tell</div>
+			<div>tell</div>
+		</div>
+	);
+};
+
+export default TodoList;
